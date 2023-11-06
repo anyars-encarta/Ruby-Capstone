67187feeac4dda32e68242812cc93a858bf6f5ee
BiancaMe <bianquis0410@gmail.com>
2023-11-06 12:24:17 -0300

    Create item class task completed

--- lib/item.rb
+++ lib/item.rb
@@ -1,17 +1,32 @@
+# frozen_string_literal: true
+
 class Item
-  def initialize(id, genre, author, source, label, publish_date, archived)
+  attr_accessor :gener, :author, :source, :label
+  attr_reader :id, :publish_date, :archived
+
+  def initialize(id, publish_date, archived)
     @id = id
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
     @publish_date = publish_date
     @archived = archived
   end
 
-  def can_be_archived
+  def save_genre(genre)
+    @genre = genre
   end
 
-  def move_to_archived
+  def save_author(author)
+    @author = author
   end
+
+  def save_source(source)
+    @source = source
+  end
+
+  def save_label(label)
+    @label = label
+  end
+
+  def can_be_archived; end
+
+  def move_to_archived; end
 end
