111f643e32dd17e2f8818bcc31ac477f37e978e4
BiancaMe <bianquis0410@gmail.com>
2023-11-07 09:23:49 -0300

    Fix linters errors

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,13 +1,17 @@
-class Genre 
-    attr_reader :id
-    attr_accessor :name, :items
-    def initialization(name)
-        @id = rand(1..500)
-        @name = name
-        @items=[]
-    end
+# frozen_string_literal: true
 
-    def add_item(item)
-        @items << item
-    end
+# Genre class
+class Genre
+  attr_reader :id
+  attr_accessor :name, :items
+
+  def initialization(name)
+    @id = rand(1..500)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
 end
