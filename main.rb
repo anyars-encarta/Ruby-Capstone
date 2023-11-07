c5e912e6a1fba65a6b796472c1dd76d1f6094fc0
BiancaMe <bianquis0410@gmail.com>
2023-11-07 08:01:45 -0300

    Fix rubocop errors

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'app'
 require_relative 'menu'
 
@@ -5,22 +7,12 @@ require_relative 'menu'
 class Main
   def initialize
     @app = App.new
-    @choices = {
-      1 => -> { @app.list_all_books },
-      2 => -> { @app.list_all_music_albums },
-      3 => -> { @app.list_of_games },
-      4 => -> { @app.list_all_genres },
-      5 => -> { @app.list_all_labels },
-      6 => -> { @app.list_all_authors },
-      7 => -> { @app.add_a_book },
-      8 => -> { @app.add_a_music_album },
-      9 => -> { @app.add_a_game },
-      10 => -> { quit }
-    }
   end
 
   def main
     loop do
+      puts 'Welcome to our Catalog App!'
+      puts 'Please select an option:'
       display_menu
       choice = gets.chomp.to_i
       handle_choice(choice)
--- menu.rb
+++ menu.rb
@@ -5,8 +5,6 @@ require_relative 'app'
 private
 
 def display_menu
-  puts 'Welcome to our Catalog App!'
-  puts 'Please select an option:'
   puts '1. List all books'
   puts '2. List all music albums'
   puts '3. List of games'
@@ -20,10 +18,31 @@ def display_menu
 end
 
 def handle_choice(choice)
-  if @choices.key?(choice)
-    @choices[choice].call
+  case choice
+  when 1..6 then choice_list(choice)
+  when 7, 8, 9 then choice_add(choice)
+  when 10 then quit
   else
     invalid_choice
+  end
+end
+
+def choice_add(choice)
+  case choice
+  when 7 then @app.add_a_book
+  when 8 then  @app.add_a_music_album
+  when 9 then  @app.add_a_game
+  end
+end
+
+def choice_list(choice)
+  case choice
+  when 1 then  @app.list_all_books
+  when 2 then  @app.list_all_music_albums
+  when 3 then @app.list_of_games
+  when 4 then @app.list_all_genres
+  when 5 then @app.list_all_labels
+  when 6 then @app.list_all_authors
   end
 end
 
