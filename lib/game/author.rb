c8f43704e5edc9ff9c672c8d143bb7478a7240fc
Anyars Yussif <anyarsencarta@gmail.com>
2023-11-08 14:38:04 +0000

    Remove comment in Author class

--- lib/game/author.rb
+++ lib/game/author.rb
@@ -31,16 +31,6 @@ class Author
     }.to_json
   end
 
-  # def self.from_json(json_data)
-  #   author_data = JSON.parse(json_data)
-  #   author = new(author_data['id'], author_data['first_name'], author_data['last_name'])
-  #   author_data['items'].each do |item_id|
-  #     item = Item.all.find { |i| i.id == item_id }
-  #     author.add_item(item) if item
-  #   end
-  #   author
-  # end
-
   def self.from_json(json_data)
     author = JSON.parse(json_data)
     new(author['id'], author['first_name'], author['last_name'])
