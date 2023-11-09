81d2e98fae67053da1e29d97ec05c2ebb51bda83
BiancaMe <bianquis0410@gmail.com>
2023-11-08 22:22:44 -0300

    Add genre table and musicAlbum table in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE catalog;
+
 -- Games
 CREATE TABLE games (
   item_id INT REFERENCES items(id) ON DELETE CASCADE,
@@ -17,5 +19,28 @@ CREATE TABLE items (
   id INT PRIMARY KEY,
   author_id INT REFERENCES authors(id) ON DELETE CASCADE,
   publish_date DATE,
-  archived BOOLEAN DEFAULT FALSE
+  archived BOOLEAN DEFAULT FALSE,
+  genre_id integer,
+  FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
 );
+
+-- Music Albums
+CREATE TABLE MUSICALBUM (
+  id integer GENERATED ALWAYS AS IDENTITY,
+  on_spotify BOOLEAN,
+  genre_id integer,
+  item_id integer,
+  PRIMARY KEY (id),
+  FOREIGN KEY(genre_id) REFERENCES GENRE (id) ON DELETE CASCADE,
+  FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
+);
+
+-- Genre
+CREATE TABLE GENRE (
+  id integer GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  music_id integer,
+  PRIMARY KEY (id),
+  FOREIGN KEY(music_id) REFERENCES MUSICALBUM (id) ON DELETE CASCADE,
+);
+
